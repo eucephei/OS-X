fe30b256f6608a881cde1c51ebe1bbf981b51e7b
eucephei <eucephei@gmail.com>
2011-12-19 18:30:03 -0800

    meeting.TotalBillingRate binding

--- Meet_Tracker/Meet_Tracker/PrefsWindow.m
+++ Meet_Tracker/Meet_Tracker/PrefsWindow.m
@@ -24,11 +24,13 @@
     [super dealloc];
 }
 
+/*
 - (void)windowDidLoad
 {
     [super windowDidLoad];
     
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
 }
+*/
 
 @end
